e8218405decf8b4918818cf0a9b8a463b0c53911
jagapatr <jagapatr@motorola.com>
2023-02-16 10:53:22 -0300

    First commit

---
+++ MQSRequestData/Application.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace MQSRequestData
+{
+    partial class Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(150, 12);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(52, 31);
+            this.buttonRun.TabIndex = 0;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(23, 69);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(52, 13);
+            this.labelStatus.TabIndex = 1;
+            this.labelStatus.Text = "Status.....";
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 116);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonRun);
+            this.Name = "Application";
+            this.Text = "MQSRequestData";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Label labelStatus;
+    }
+}
+
---
+++ MQSRequestData/Application.cs
@@ -0,0 +1,38 @@
+﻿using GlobalOperations.Definitions;
+using MQSInterceptClass;
+using MQSRequestDataYield;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MQSRequestData
+{
+    public partial class Application : Form
+    {
+        public Application()
+        {
+            InitializeComponent();
+        }
+        string url = "mqs.motorola.com";
+        string home = "MQS Home";
+        string erroMsg = string.Empty;
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            List<MqsDefinitions.TestProcess> UnitInfoResults;
+            MQSGetYield mGY = new MQSGetYield();
+
+            erroMsg = mGY.QueryYieldThread(url, out UnitInfoResults, home);
+            if (erroMsg == string.Empty)
+                labelStatus.Text = "Page OK!";
+            else
+                labelStatus.Text = "Page Error: " + erroMsg;
+        }
+    }
+}
